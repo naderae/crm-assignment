8cc9063fb078c4006c460c505a30472c23422449
naderae <naderabouezze93@gmail.com>
2017-07-10 13:32:38 -0400

    1st

--- contact.rb
+++ contact.rb
@@ -1,4 +1,12 @@
+gem 'activerecord', '=4.2.7'
+require 'active_record'
+require 'mini_record'
+
+ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'crm.sqlite3')
+
+
 class Contact
+
 
 @@contact_list = []
 @@id = 1
